8b6c07bba508de72984cc7848b706d7c15296e00
Robert Ścieszka <85113688+Edishakiri@users.noreply.github.com>
2021-09-25 14:35:23 +0200

    Create WidgetLg.jsx

---
+++ admin/src/components/widgetLg/WidgetLg.jsx
@@ -0,0 +1,82 @@
+import "./widgetLg.css";
+
+export default function WidgetLg() {
+  const Button = ({ type }) => {
+    return <button className={"widgetLgButton " + type}>{type}</button>;
+  };
+  return (
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Ostatnie transakcje</h3>
+      <table className="widgetLgTable">
+        <tbody>
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Użytkownik</th>
+          <th className="widgetLgTh">Data</th>
+          <th className="widgetLgTh">Koszt</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Robert Lewandowski</span>
+          </td>
+          <td className="widgetLgDate">2 Lip 2021</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Robert Lewandowski</span>
+          </td>
+          <td className="widgetLgDate">2 Lip 2021</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Declined" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Robert Lewandowski</span>
+          </td>
+          <td className="widgetLgDate">2 Lip 2021</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Pending" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt=""
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Robert Lewandowski</span>
+          </td>
+          <td className="widgetLgDate">2 Lip 2021</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
